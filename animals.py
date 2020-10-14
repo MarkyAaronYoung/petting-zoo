ef8426545c9db7ed5d9726efcc97dea369c2e39f
Mark Young <markyaaronyoung@gmail.com>
2020-10-13 21:31:08 -0500

    can successfully print an instance of an almond

--- animals.py
+++ animals.py
@@ -127,6 +127,3 @@ class Bass:
         self.species = species
         self.date_added = date.today()
         self.swimming = True
-
-    def __str__(self):
-        return f"My name is {self.name} and I am a {self.species}."
