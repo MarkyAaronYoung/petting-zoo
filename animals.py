c28070381758f1473f773df816236dc9dbc5849d
Mark Young <markyaaronyoung@gmail.com>
2020-10-17 13:44:30 -0500

    added stringify and feed methods to each critter

--- animals.py
+++ animals.py
@@ -15,3 +15,11 @@ from python_snake import Python_Snake
 from goose import Goose
 from swan import Swan
 from bass import Bass
+
+
+franny = Llama("Franny", "Classic Llama", "midday", "seeds")
+sliver = CopperHead("Sliver", "Snake", "mice")
+billy_bob = Bass("Billy Bob", "fish", "chum")
+lil_nut = Almond("Lil Nut", "nut", "minerals")
+thlither = CopperHead("Thlither", "snake", "mice")
+print(thlither)
