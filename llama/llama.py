2b996af2280b38b886f983a7cb72935fe17a0b2f
Mark Young <markyaaronyoung@gmail.com>
2020-10-26 19:41:52 -0500

    created class animal parent class and used it to establish inheritance in all the critter classes then created an overriding feed function for each critter

--- llama/llama.py
+++ llama/llama.py
@@ -1,16 +1,11 @@
 from datetime import date
+from animal import Animal
 
-class Llama:
-    def __init__(self, name, species, shift, food):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.walking = True
-        self.shift = shift
-        self.food = food
-    
-    def feed(self):
-      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
-
-    def __str__(self):
-        return f"{self.name} is a {self.species}"
+class Llama(Animal):
+  def __init__(self, name, species, shift, food, chip_num):
+      super().__init__(name, species, food, chip_num)
+      self.shift = shift
+      self.walking = True
+  
+  def feed(self):
+      print(f'on {date.today()}, {self.name} had "Rockytop" sung to it so it would eat its {self.food}')
